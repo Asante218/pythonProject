f48c84da04a66bb9050a192fde436de9884c4836
Asante <djplays604@gmail.com>
2021-06-18 15:31:02 +0400

    python practice

---
+++ programOne.py
@@ -0,0 +1,30 @@
+#author: asante
+#task: print the 5 6 times
+number = 9
+while(number<=12):
+	number += 1   
+
+
+	if number < 11:
+		print( "Asante is jumping")
+
+	elif ((number> 10) and (number< 12)):
+		print ("Asante is riding")
+
+	elif ((number>= 12) and (number< 13)):
+		print ("Asante is climbing")
+    
+	else: 
+		print( "Asante is swiming")
+
+	
+var = 'y'
+  
+if (var =='Y' or var =='y'):
+    print("YOU SAID YES")
+elif(var =='N' or var =='n'):
+    print("YOU SAID NO")
+else:
+    print("INVALID INPUT")
+
+   
