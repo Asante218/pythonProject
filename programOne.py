6d2bc6ca5e13abbccefec5fde3a363dc66a3ee02
Asante <djplays604@gmail.com>
2021-06-19 10:28:42 +0400

    printing a list

--- programOne.py
+++ programOne.py
@@ -1,30 +1,7 @@
 #author: asante
-#task: print the 5 6 times
-number = 9
-while(number<=12):
-	number += 1   
 
 
-	if number < 11:
-		print( "Asante is jumping")
 
-	elif ((number> 10) and (number< 12)):
-		print ("Asante is riding")
-
-	elif ((number>= 12) and (number< 13)):
-		print ("Asante is climbing")
-    
-	else: 
-		print( "Asante is swiming")
-
-	
-var = 'o'
-  
-if (var =='Y' or var =='y'):
-    print("YOU SAID YES")
-elif(var =='N' or var =='o'):
-    print("YOU SAID NO")
-else:
-    print("INVALID INPUT")
-
-   
+a = ["car","pot", "bed", "eat"]
+for x in a:
+  print(x) 
