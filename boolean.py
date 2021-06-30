6bf63acb1b4c18564f1b061215ba0701f8076f42
Asante <djplays604@gmail.com>
2021-06-30 16:37:33 +0400

    checking if three numbers are equal

--- boolean.py
+++ boolean.py
@@ -1,51 +1,3 @@
-first = "hello world"
-
-
-
-
-
-#boolean exercise
-
-print("i am a computer")
-
-#Write an if statement that checks if 1 is less than 2 and if 4 is greater than 2. If it is, show the message "Math is fun."`
-
-if 1 < 2 and 4 > 2:
-	print("math is fun ")
-
-nope = ""
-
-name = input("enter your name : ")
-
-print("hello, " + name )
-
-
-
-#Ask the user for a number. If the number is negative, show a message that says "That number is less than 0!" If the number is positive, show a message that says "That number is greater than 0!" Otherwise, show a message that says "You picked 0!.
-
-
-number = int(input("enter a number :"))
-
-if number > 0:
-	print("That number is greater than" , 0 , "!")
-
-elif number == 0:
-	print("You picked " , 0 , "!.")
-
-else:
-	print("That number is less than " , 0 ,  "!")
-
-
-print("see if the numbers are eqaul")
-
-x = int(input("insert number one: " ))
-y = int(input("insert number two: " ))
-
-if x == y:
-	print(True)
-else:
-	print(False)
-
 
 print("Insert three numbers to see if they are equal.")
 
@@ -64,16 +16,18 @@ s = int(input("insert third number : " ))
 if f == d == s:
 	print(True)
 
-elif f == s:
-	print("f and s are equal")
+elif f == s or f == d or d == s:
+	if f == s:
+		print("f is equal to s")
+
+	elif d == s:
+		print("d is equal to s")
+
+	else:
+		print("f is equal to d")
 
 
-elif f == d:
-	print("f and d are equal")
-
-
-elif d == s:
-	print("d and s are equal")
+	
 
 
 else:
