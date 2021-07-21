50412df584d164acd1f20a8ac965e9d435079672
Asante <djplays604@gmail.com>
2021-07-21 11:30:01 +0400

    class and objects

--- functionsPractice.py
+++ functionsPractice.py
@@ -59,22 +59,12 @@ def singleLetterCount(g , good):
 
 print(singleLetterCount(letter , word))
 
+def f(a, b = 1, c = 2):
+   print('a is: ',a, 'b is: ', b, 'c is: ', c)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("this is for function f")
+f(2, c = 2)
+f(c = 100, a = 110)
 
