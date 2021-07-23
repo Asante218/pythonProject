76c9ecfc316e4a4df88db13757d5d073887c84eb
Asante <djplays604@gmail.com>
2021-07-23 12:01:33 +0400

    exerxise questions

--- classes.py
+++ classes.py
@@ -1,59 +1,21 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-
-p1 = Person("John", 36)
-
-print(p1.name)
-print(p1.age)
-
-
-
-#Exercise
-#We have a class defined for vehicles. Create two new vehicles called car1 and car2. Set car1 to be a red convertible worth $60,000.00 with a name of Fer, and car2 to be a blue van named Jump worth $10,000.00.
-
-
-
-
-
-# define the Vehicle class
-class Vehicle:
-    name = ""
-    kind = "car"
-    color = ""
-    value = 100.00
-    def description(self):
-        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
-        return desc_str
-# your code goes here
-
-car1 = Vehicle()
-car1.name = "Fer"
-car1.color = "red"
-car1.kind = "convertible"
-car1.value = 60000.00
-
-car2 = Vehicle()
-car2.name = "Jump"
-car2.color = "blue"
-car2.kind = "van"
-car2.value = 10000.00
-
-
-# test code
-print(car1.description())
-print(car2.description())
 
 #Write a Python program to create a class and print your name as 
 # a variable in the class and print your age using a function in the class
 
 class PersonInfo:
-  name = Asante
+  personName = "Asante"
 
-  def age():
+  def age(self):
     return 9 
 
 people = PersonInfo()
-print(people.name())
+print(people.personName)
 print(people.age())
+
+class A:
+  def __init__(self,b):
+    self.b=b
+  def display(self):
+    print(self.b)
+obj=A("Hello")
+del obj
